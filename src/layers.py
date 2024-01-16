6d58eece13897bc97de6e908aff5f77a11bf9e6a
Alireza <alirezarafieieng@gmail.com>
2024-01-16 10:29:26 -0500

    Commit

--- src/layers.py
+++ src/layers.py
@@ -3,11 +3,11 @@ import keras
 weights_initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.02, seed=2024)
 
 
-def conv1d(filters, kernel_size, strides=1, padding='same', activation=None, use_bias = True):
+def Conv1D(filters, kernel_size, strides=1, padding='same', activation=None, use_bias = True):
 
     layer = keras.layers.Conv1D(
-        filters, kernel_size, strides=1, padding="valid", data_format=None,
-        dilation_rate=1, groups=1, activation=None, use_bias=True, kernel_initializer=weights_initializer,
+        filters, kernel_size, strides=strides, padding=padding, data_format=None,
+        dilation_rate=1, groups=1, activation=activation, use_bias=use_bias, kernel_initializer=weights_initializer,
         bias_initializer="zeros", kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
         kernel_constraint=None, bias_constraint=None
     )
@@ -15,10 +15,10 @@ def conv1d(filters, kernel_size, strides=1, padding='same', activation=None, use
     return layer
 
 
-def batchNormalization(trainable=True, virtual_batch_size=None):
+def BatchNormalization(momentum=0.99, epsilon=0.001, trainable=True, virtual_batch_size=None):
 
     layer = keras.layers.BatchNormalization(
-        axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True,
+        axis=-1, momentum=momentum, epsilon=epsilon, center=True, scale=True,
         beta_initializer='zeros', gamma_initializer='ones',
          moving_mean_initializer='zeros', moving_variance_initializer='ones',
         beta_regularizer=None, gamma_regularizer=None, beta_constraint=None,
@@ -29,7 +29,7 @@ def batchNormalization(trainable=True, virtual_batch_size=None):
     return layer
 
 
-def activation(activation):
+def Activation(x, activation):
     
     if activation == 'relu':
         return keras.activations.relu(x, alpha=0.0, max_value=None, threshold=0)
@@ -44,4 +44,13 @@ def activation(activation):
     elif activation == 'tanh':
         return keras.activations.tanh(x)
     else:
-        raise ValueError('please check the name of the activation')
+        raise ValueError('please check the name of the activation function!')
+    
+
+def MaxPooling1D(pool_size=2, strides=None, padding="valid"):
+
+    layer = keras.layers.MaxPooling1D(
+    pool_size=pool_size, strides=strides, padding=padding, data_format=None, name=None
+    )
+
+    return layer
--- src/modules.py
+++ src/modules.py
@@ -1,16 +1,63 @@
 import layers
-from keras.layers import Input, Add, MaxPooling1D, UpSampling1D, concatenate, Multiply, GlobalAveragePooling1D
+from keras.layers import Input, Add, UpSampling1D, concatenate, Multiply, GlobalAveragePooling1D
+
 
 def recurrent_residual_conv1d(input_layer, filters, kernel_size=3):
     # First Convolution
-    conv = conv1d(filters, kernel_size, padding='same', stride=1)(input_layer)
-    conv = batchNormalization()(conv)
-    conv = activation('relu')(conv)
+    conv = Covid1D(filters, kernel_size, padding='same', stride=1)(input_layer)
+    conv = BatchNormalization()(conv)
+    conv = Activation(conv, 'relu')
 
     # Recurrent Layer
-    recurrent = conv1d(filters, kernel_size, padding='same', activation='relu')(conv)
+    recurrent = Covid1D(filters, kernel_size, padding='same', activation='relu')(conv)
     
     # Residual Connection
     residual = Add()([input_layer, recurrent])
 
     return residual
+
+
+def attention_block(x, g, inter_channel):
+    # Attention Block
+    theta_x = Covid1D(inter_channel, kernel_size=1, padding='same')(x)
+    phi_g = Covid1D(inter_channel, kernel_size=1, padding='same')(g)
+
+    f = Activation('relu')(Add()([theta_x, phi_g]))
+    psi_f = Covid1D(1, kernel_size=1, padding='same', activation='sigmoid')(f)
+
+    return Multiply()([x, psi_f])
+
+
+def R2U_Net(input_size, depth, initial_features, kernel_size):
+    inputs = Input(input_size)
+
+    # Encoder path
+    encoders = []
+    for i in range(depth):
+        filters = initial_features * (2 ** i)
+        if filters >= 512: filters = 512
+        if i == 0:
+            x = recurrent_residual_conv1d(inputs, filters, kernel_size)
+        else:
+            x = recurrent_residual_conv1d(encoders[-1][0], filters, kernel_size)
+        p = MaxPooling1D(pool_size=2)(x)
+        encoders.append((x, p))
+
+    # Bottleneck
+    bottleneck = recurrent_residual_conv1d(encoders[-1][1], 512, kernel_size)
+
+    # Decoder path
+    for i in reversed(range(depth)):
+        filters = initial_features * (2 ** i)
+        if filters >= 512: filters = 512
+        if filters <= 64: filters = 64
+        g = UpSampling1D(size=2)(bottleneck if i == depth - 1 else x)
+        attn = attention_block(encoders[i][0], g, filters)
+        x = concatenate([g, attn])
+        x = recurrent_residual_conv1d(x, filters, kernel_size)
+
+    # Output layer
+    output = Conv1D(1, kernel_size, strides=2, padding='same', activation='tanh')(x)
+
+    model = Model(inputs=inputs, outputs=output)
+    return model
