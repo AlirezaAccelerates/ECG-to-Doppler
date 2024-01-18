86be68bc63facd235a0e291c279ab19d3feb1fe6
Alireza <alirezarafieieng@gmail.com>
2024-01-17 21:41:51 -0500

    Commit

--- src/main.py
+++ src/main.py
@@ -2,5 +2,10 @@ import layers
 import modules
 from keras.models import Model
 
+
+batch_size = 64
+num_channels = 1
+num_classes = 0
+
 generator_in_channels = latent_dim + num_classes
 discriminator_in_channels = num_channels + num_classes
